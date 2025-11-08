5bd07acf78bbaa1914bc4b3030f148f70da42f26
carolineolvs <carolineolvs0@gmail.com>
2025-11-08 20:27:34 -0300

    Configuração do Xunit, correção problemas de xUnit e remove duplicidade de AssemblyAttributes

---
+++ tests/Back_ColheitaSolidaria.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Back_ColheitaSolidaria.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void TestaSeVerdadeiroEhVerdadeiro()
+		{
+			Assert.True(true);
+		}
+	}
+}
